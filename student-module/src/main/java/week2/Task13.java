207c2cb452fb73846aaae83031b51ca6ec711ea5
snek007 <vovanchell@gmail.com>
2018-11-06 11:27:31 +0200

    week2 Task 13 done

--- student-module/src/main/java/week2/Task13.java
+++ student-module/src/main/java/week2/Task13.java
@@ -2,12 +2,12 @@ package week2;
 
 /**
  * 13. Последовательность Фибоначчи определяется так:
- *
+ * <p>
  * φ0=0,  φ1=1, ..., φn=φn-1+φn-2.
- *
+ * <p>
  * По данному числу n определите n-е число Фибоначчи φn и вывести его на экран.
  * Число будет >= 0.
- *
+ * <p>
  * φ7 -> 13
  * φ10 -> 55
  */
@@ -15,8 +15,16 @@ public class Task13 {
     public static void main(String[] args) {
 
         int number = Integer.parseInt(args[0]);
-
-        //        Yours code...
-
+        if (number == 0) {
+            System.out.println("0");
+        } else {
+            int[] masFibNum = new int[number + 1];
+            masFibNum[0] = 0;
+            masFibNum[1] = 1;
+            for (int i = 2; i < masFibNum.length; i++) {
+                masFibNum[i] = masFibNum[i - 1] + masFibNum[i - 2];
+            }
+            System.out.println(masFibNum[number]);
+        }
     }
 }
